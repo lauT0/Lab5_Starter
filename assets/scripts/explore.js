c7a4fbdbaf4984d6289eba9c44b0d05e69050c45
Sam <s9lau@ucsd.edu>
2024-05-05 21:23:33 -0700

    talking with setInterval implementation

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -7,29 +7,48 @@ function init() {
   const synth = window.speechSynthesis;
   const select = document.getElementById("voice-select");
   const button = document.querySelector("button");
+  let image = document.querySelector("img");
   let text = document.querySelector("textarea");
-  let voices = synth.getVoices();
-  for (let i = 0; i < voices.length; i++){
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("value", voices[i].name);
-    select.appendChild(option);
-  }
-  console.log(voices);
-  select.addEventListener("change", (event)=>{
-    let voice = event.target.value;
-    for (let i = 0; i < voices.length; i++) {
-      if (voices[i].name === voice) {
-        utterThis.voice = voices[i];
-      }
+  let voices = [];
+  let voice;
+
+  window.onload = function populate() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++){
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("value", voices[i].name);
+      select.appendChild(option);
     }
-    console.log(voice);
+    console.log(voices);
+  }
+  //synth.addEventListener("voiceschanged", populate);
+  
+  select.addEventListener("change", (event)=>{
+    voice = event.target.value;
   });
 
   button.addEventListener("click", () => {
     let phrase = new SpeechSynthesisUtterance(text.value);
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === voice) {
+        phrase.voice = voices[i];
+      }
+    }
+    console.log(voice);
     synth.speak(phrase);
   });
+
+  function talking() {
+    if(synth.speaking === true){
+      image.src = "assets/images/smiling-open.png";
+    }
+    else {
+      image.src = "assets/images/smiling.png";
+    }
+  };
+
+  setInterval(talking, 1);
 
 }
